ea9ab1d3a19695a73707bd8f310cd287d8ec6ef3
stJhimy <stjhimy@gmail.com>
2013-07-04 14:07:32 -0300

    Add make_structured_list helper

--- lib/mws-rb/helpers.rb
+++ lib/mws-rb/helpers.rb
@@ -7,5 +7,9 @@ module MWS
     def self.escape_date_time_params(hash)
       hash.map{|key, value| {key => [Time, Date, DateTime].include?(value.class) ? value.iso8601 : value}}.reduce({}, :merge)
     end
+
+    def self.make_structured_list(name, items)
+      items.map.with_index{|item, index| {:"#{name}#{index + 1}" => item}}.reduce({}, :merge)
+    end
   end
 end
