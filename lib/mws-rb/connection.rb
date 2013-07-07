ba078985b04b5e96c82b95d65a7ce9409ddaf4cb
stJhimy <stjhimy@gmail.com>
2013-07-07 04:15:49 -0300

    Remove marketplace_id from initialization

--- lib/mws-rb/connection.rb
+++ lib/mws-rb/connection.rb
@@ -1,13 +1,12 @@
 module MWS
   class Connection
-    attr_reader :host, :aws_access_key_id, :aws_secret_access_key, :merchant_id, :marketplace_id
+    attr_reader :host, :aws_access_key_id, :aws_secret_access_key, :merchant_id
 
     def initialize(options={})
       @host = options[:host]
       @aws_access_key_id = options[:aws_access_key_id]
       @aws_secret_access_key = options[:aws_secret_access_key]
       @merchant_id = options[:merchant_id]
-      @marketplace_id = options[:marketplace_id]
     end
 
     def get(uri)
