71862c64b8bfc8f4f9e4103d1d36043d4ea312dd
stJhimy <stjhimy@gmail.com>
2013-07-11 17:57:31 -0300

    Add Actions constant to products api

--- lib/mws-rb/api/products.rb
+++ lib/mws-rb/api/products.rb
@@ -1,6 +1,13 @@
 module MWS
   module API
     class Products < Base
+      Actions = [:list_matching_products, :get_matching_product, :get_matching_product_for_id,
+                 :get_competitive_pricing_for_SKU, :get_competitive_pricing_for_ASIN,
+                 :get_lowest_offer_listings_for_SKU, :get_lowest_offer_listings_for_ASIN,
+                 :get_my_price_for_SKU, :get_my_price_for_ASIN,
+                 :get_product_categories_for_SKU, :get_product_categories_for_ASIN,
+                 :get_service_status]
+
       def initialize(connection)
         @uri = "/Products/2011-10-01"
         @version = "2011-10-01"
