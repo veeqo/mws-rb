4bd02083add9bd312fdb442f03393521fd60c73e
stJhimy <stjhimy@gmail.com>
2013-07-25 07:39:00 -0300

    Add feeds actions

--- lib/mws-rb/api/feeds.rb
+++ lib/mws-rb/api/feeds.rb
@@ -1,6 +1,9 @@
 module MWS
   module API
     class Feeds < Base
+      Actions = [:submit_feed, :get_feed_submission_list, :get_feed_submission_list_by_next_token,
+                 :get_feed_submission_count, :cancel_feed_submissions, :get_feed_submission_result ]
+
       def initialize(connection)
         @uri = "/"
         @version = "2009-01-01"
