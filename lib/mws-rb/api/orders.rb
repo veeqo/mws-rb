210b3c8af5268ae615b71d7cbb0c0592d671a095
stJhimy <stjhimy@gmail.com>
2013-07-25 07:31:53 -0300

    Add orders actions

--- lib/mws-rb/api/orders.rb
+++ lib/mws-rb/api/orders.rb
@@ -1,6 +1,9 @@
 module MWS
   module API
     class Orders < Base
+      Actions = [:list_orders, :list_orders_by_next_token, :get_order, :list_order_items,
+                 :list_order_items_by_next_token, :get_service_status]
+
       def initialize(connection)
         @uri = "/Orders/2011-01-01"
         @version = "2011-01-01"
