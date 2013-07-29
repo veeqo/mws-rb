c39f2c6fdae0b872f4df8a840dd1d029eb1a8c50
stJhimy <stjhimy@gmail.com>
2013-07-29 10:00:52 -0300

    Add recommendations actions

--- lib/mws-rb/api/recommendations.rb
+++ lib/mws-rb/api/recommendations.rb
@@ -1,6 +1,8 @@
 module MWS
   module API
     class Recommendations < Base
+      Actions = [:get_last_updated_time_for_recommendations_checks, :list_recommendations, :list_recommendations_by_next_token]
+
       def initialize(connection)
         @uri = "/Recommendations/2013-04-01"
         @version = "2013-04-01"
