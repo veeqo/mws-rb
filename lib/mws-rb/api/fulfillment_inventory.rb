f314a7e0fb1aba0cb349b907710345019344b7de
stJhimy <stjhimy@gmail.com>
2013-07-30 07:00:22 -0300

    Add fulfillment_inventory actions

--- lib/mws-rb/api/fulfillment_inventory.rb
+++ lib/mws-rb/api/fulfillment_inventory.rb
@@ -1,6 +1,8 @@
 module MWS
   module API
     class FulfillmentInventory < Base
+      Actions = [:list_inventory_supply, :list_inventory_supply_by_next_token, :get_service_status]
+
       def initialize(connection)
         @uri = "/FulfillmentInventory/2010-10-01"
         @version = "2010-10-01"
