c25474f32cdbbe55add2191b7d398517ecdbdbd5
Ryan D Johnson <ryandjohnson@gmail.com>
2014-01-04 13:49:16 -0800

    Add Actions for FulfillmentOutboundShipment

--- lib/mws-rb/api/fulfillment_outbound_shipment.rb
+++ lib/mws-rb/api/fulfillment_outbound_shipment.rb
@@ -1,6 +1,17 @@
 module MWS
   module API
     class FulfillmentOutboundShipment < Base
+      Actions = [
+        :cancel_fulfillment_order,
+        :create_fulfillment_order,
+        :get_fulfillment_order,
+        :get_fulfillment_preview,
+        :get_package_tracking_details,
+        :get_service_status,
+        :list_all_fulfillment_orders,
+        :list_all_fulfillment_orders_by_next_token,
+      ]
+
       def initialize(connection)
         @uri = "/FulfillmentOutboundShipment/2010-10-01"
         @version = "2010-10-01"
