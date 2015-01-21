f5fd6e9b6940172c68f72b52cde8171c9bc1c575
David La Chasse <david.lachasse@gmail.com>
2015-01-21 10:50:28 -0800

    Update order API version

--- lib/mws-rb/api/orders.rb
+++ lib/mws-rb/api/orders.rb
@@ -5,8 +5,8 @@ module MWS
                  :list_order_items_by_next_token, :get_service_status]
 
       def initialize(connection)
-        @uri = "/Orders/2011-01-01"
-        @version = "2011-01-01"
+        @uri = "/Orders/2013-09-01"
+        @version = "2013-09-01"
         @verb = :post
         super
       end
