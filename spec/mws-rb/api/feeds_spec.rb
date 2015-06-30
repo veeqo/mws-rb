d11dd022aaf2196efbaa89855760b80cbe554503
Yevhen V <wik@bdhr.co>
2015-06-30 15:20:24 +0300

    Add specs to illustrate the problem with response format
    1. Generally it is a good idea to force MWS response format to XML,
    because they always responding with it, regardless of content-type
    specified in the body of the response;
    2. HTTParty requires format option to be a symbol.

--- spec/mws-rb/api/feeds_spec.rb
+++ spec/mws-rb/api/feeds_spec.rb
@@ -21,4 +21,68 @@ describe MWS::API::Feeds do
   it "should set the right :version" do
     feeds.version.should eq("2009-01-01")
   end
+
+  describe "#submit_feed" do
+    let(:mws_api) {
+      MWS.new(
+        host: "mws-eu.amazonservices.com",
+        aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+        aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+        seller_id: ENV['AWS_SELLER_ID']
+      )
+    }
+
+    subject { mws_api.feeds.submit_feed(feed_data) }
+
+    context "with xml request", vcr: { cassette_name: 'feeds/submit_feed/xml_request' } do
+      let(:feed_data) {
+        {
+          feed_type: "_POST_INVENTORY_AVAILABILITY_DATA_",
+          message_type: :inventory,
+          messages: [
+            {
+              "MessageID" => (Time.now.to_i * rand).ceil.to_s,
+              "OperationType" => "Update",
+              "Inventory" => {
+                "SKU" => 'LG-WHITE-1389247',
+                "Quantity" => '9'
+              }
+            }
+          ]
+        }
+      }
+
+      it 'returns a kind of hash' do
+        expect(subject).to be_kind_of(Hash)
+      end
+    end
+
+    context "with text request", vcr: { cassette_name: 'feeds/submit_feed/text_request' } do
+      let(:feed_data) {
+        {
+          feed_type: "_POST_FLAT_FILE_INVLOADER_DATA_",
+          type: 'text',
+          message: "sku\tquantity\nLG-WHITE-1389247\t9"
+        }
+      }
+
+      it 'returns a kind of hash' do
+        expect(subject).to be_kind_of(Hash)
+      end
+    end
+
+    context "with invalid request", vcr: { cassette_name: 'feeds/submit_feed/invalid_request' } do
+      let(:feed_data) {
+        {
+          # feed_type is missed
+          type: 'text',
+          message: "sku\tquantity\nLG-WHITE-1389247\t9"
+        }
+      }
+
+      it 'returns a kind of hash' do
+        expect(subject).to be_kind_of(Hash)
+      end
+    end
+  end
 end
